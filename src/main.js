9d7884b647f6b1dda413b122b88dd4f352b92ed3
Pongsak Prabparn <pongsak@Pongsaks-MacBook-Pro.local>
2018-11-16 12:42:54 +0700

    [+] env : adding env test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+require('dotenv').load()
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount('#app')
+
+console.log(process.env)
